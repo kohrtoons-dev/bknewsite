dc26c5345c91493249ba88c96a0f5a0e785ede45
kohrtoons-dev <kohrtoons@gmail.com>
2026-09-14 09:13:53 -0400

    Update customer testimonials

--- includes/reviews.php
+++ includes/reviews.php
@@ -6,18 +6,18 @@
 <div class="reviews">
 <div class="review">
 <div aria-label="5 out of 5 stars" class="stars">&#9733;&#9733;&#9733;&#9733;&#9733;</div>
-<p>Knowledgeable, credited professional and fun traders. Both Kathy and Boris. Always update on what's going on in the markets with the hows and whys. Learnt lots from them. Been with them for almost 4 years now. Lifetime member. Value for their experience is definitely worth it. Whenever I thought I learn or knew everything on TA. Boris will always come out with something new, insightful and good. Especially by having skin the game together with us, he don't just talk the talk. Coaching session with Boris is most useful in my opinion. The mindset the psy. No indicators can beat that. Cheers.</p>
-<div class="who">by ThorZzZ</div>
-</div>
-<div class="review">
-<div aria-label="4 out of 5 stars" class="stars">&#9733;&#9733;&#9733;&#9733;<span class="star-empty">&#9734;</span></div>
-<p>Kathy is a wealth of knowledge and experience with correlating the macro and micro environments with current market opportunities and risk. Boris has some great coaching tips and strategies for trading futures. It's a reputable and reasonably priced place to learn how to trade prop firms.</p>
-<div class="who">by Ken S.</div>
+<p>I was searching for someone who could teach me a structured and technically sound trading system, rather than just presenting strategies.<br/><br/>Kathy is that person.</p>
+<div class="who">by Tao Bo</div>
 </div>
 <div class="review">
 <div aria-label="5 out of 5 stars" class="stars">&#9733;&#9733;&#9733;&#9733;&#9733;</div>
-<p>What a great course! Kathy took extra time out and extended the VIP sessions, each session invaluable with her sharing her extensive knowledge. It was a small select group, around 25 of us I think, which meant she could take the time to look at our 'homework' (!) and give objective advise. The course content is very inciteful and has really given me the confidence &amp; helped me get going again. Have had a successful 2 weeks using Kathy's training, advise and indicators. Cannot ask for any more than that...</p>
-<div class="who">by Cheshire Lady</div>
+<p>I have regularly listened to Boris with his live trading - besides the entertainment value, the key benefits for me are that listening to the live trading constantly reminds me there will always be another trade so I don&rsquo;t need to chase.<br/><br/>I&rsquo;ve passed five prop accounts so far. My ideal trading involves trading ten accounts - one at a time - with small daily targets of 40 ticks per account. I trade one instrument (NQ) and simply specialise using BK indicators and price action.</p>
+<div class="who">by Karen Sanderson</div>
+</div>
+<div class="review">
+<div aria-label="5 out of 5 stars" class="stars">&#9733;&#9733;&#9733;&#9733;&#9733;</div>
+<p>Boris thank You for your awesome strategies, amazing insights and constant striving to help us improve. I am sooooooo stoked and ever grateful. Woo Hoo!!!</p>
+<div class="who">by Cheryl Murakami</div>
 </div>
 </div>
 </div>
